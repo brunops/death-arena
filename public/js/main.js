1ad008c908b14b65580943cb8e872cc765ff56fe
Bruno Sanches <brunopsanches@gmail.com>
2014-03-11 23:51:15 -0700

    Include Player and remove code from main.js (now in Player.js)

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,4 @@
-/* global Entity */
-(function () {
-  'use strict';
-
-  var Player = Entity.extend({
-    x: 50,
-    y: 50,
-    width: 30,
-    height: 52,
-    speed: 200,
-    frameCooldown: 150,
-    spriteSrc: 'public/images/heroes.png',
-    spritePositions: {
-      'down':  [[10, 12], [57, 12],  [105, 12], [57, 12]],
-      'left':  [[10, 75], [57, 75],  [105, 75], [57, 75]],
-      'right': [[10, 140], [57, 140], [105, 140], [57, 140]],
-      'up':    [[10, 205], [57, 205], [105, 205], [57, 205]]
-    },
-    direction: 'down'
-  });
-
-  window.Player = Player;
-}());
-
+/* global Player */
 window.requestAnimFrame = (function () {
   'use strict';
   return window.requestAnimationFrame       ||
