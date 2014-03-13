fc4247b1559ec4adabc720bb826f3a2560415a8e
Bruno Sanches <brunopsanches@gmail.com>
2014-03-12 22:23:57 -0700

    Override `Projectile#render` to support sprite rotation
    * Although it may be faster to copy all sprite position in the sprite
    and simply map everything, rotation is good for now
    * Store `Projectile.TO_RADIANS` to avoid calculating `Math.PI / 180` over
    and over
    * Prototypes magic is used to render the sprite
    `Entity.prototype.render.call` takes advantafe of simple call to
    `context.drawImage` being already defined in `Entity`

--- public/js/Projectile.js
+++ public/js/Projectile.js
@@ -9,6 +9,26 @@
     spriteSrc: 'public/images/fireball.png'
   });
 
+  Projectile.directionAngles = {
+    'left': 0,
+    'top': 90,
+    'right': 180,
+    'bottom': 270
+  };
+  Projectile.TO_RADIANS = Math.PI / 180;
+
+  Projectile.prototype.render = function (ctx) {
+    ctx.save();
+      var x = this.x + Projectile.width / 2,
+          y = this.y + Projectile.height / 2;
+
+      ctx.translate(x, y);
+      ctx.rotate(Projectile.TO_RADIANS * Projectile.directionAngles[this.direction]);
+      ctx.translate(-x, -y);
+      Entity.prototype.render.call(this, ctx);
+    ctx.restore();
+  };
+
   // Make Projectile globally available
   window.Projectile = Projectile;
 }());
