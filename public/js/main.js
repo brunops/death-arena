312bcfa93afccf6d9a8003cbfdce11fd0ffec025
Bruno Sanches <brunopsanches@gmail.com>
2014-03-14 19:48:38 -0700

    Remove all game logic from main.js and use Game object instead
    * Include Game.js in HTML

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-/* global Player, Projectile */
+/* global Game */
 window.requestAnimFrame = (function () {
   'use strict';
   return window.requestAnimationFrame       ||
@@ -12,95 +12,7 @@ window.requestAnimFrame = (function () {
 (function () {
   'use strict';
 
-  var player = new Player({
-    x: 100,
-    y: 200
-  });
-
-  var projectiles = [];
-
-  function updateScene(modifier) {
-    var dirChanged = false;
-
-    if (modifier < 1000) {
-      // UP
-      if (keysDown[38]) {
-        if (!dirChanged) {
-          player.direction = 'up';
-          dirChanged = true;
-        }
-
-        player.y = player.y - (player.speed * modifier);
-      }
-
-      // DOWN
-      if (keysDown[40]) {
-        if (!dirChanged) {
-          player.direction = 'down';
-          dirChanged = true;
-        }
-
-        player.y = player.y + (player.speed * modifier);
-      }
-
-      // RIGHT
-      if (keysDown[39]) {
-        if (!dirChanged) {
-          player.direction = 'right';
-          dirChanged = true;
-        }
-
-        player.x = player.x + (player.speed * modifier);
-      }
-
-      // LEFT
-      if (keysDown[37]) {
-        if (!dirChanged) {
-          player.direction = 'left';
-          dirChanged = true;
-        }
-
-        player.x = player.x - (player.speed * modifier);
-      }
-
-      // SPACE
-      if (keysDown[32]) {
-        // Centralize fireball in respect to player
-        projectiles.push(new Projectile({
-          direction: player.direction,
-          x: player.x + (Player.renderedWidth / 2 - Projectile.renderedWidth / 2),
-          y: player.y + (Player.renderedHeight / 2 - Projectile.renderedHeight / 2)
-        }));
-      }
-
-      if (dirChanged) {
-        player.update();
-      }
-
-      for (var i = 0; i < projectiles.length; ++i) {
-        switch (projectiles[i].direction) {
-          case 'up':
-            projectiles[i].y -= projectiles[i].speed * modifier;
-            break;
-          case 'down':
-            projectiles[i].y += projectiles[i].speed * modifier;
-            break;
-          case 'right':
-            projectiles[i].x += projectiles[i].speed * modifier;
-            break;
-          case 'left':
-            projectiles[i].x -= projectiles[i].speed * modifier;
-            break;
-        }
-      }
-    }
-  }
-
-  var canvas = document.getElementById('canvas'),
-      ctx = canvas.getContext('2d');
-
-  canvas.width = canvas.clientWidth;
-  canvas.height = canvas.clientHeight;
+  Game.init();
 
   var before = Date.now();
   window.requestAnimFrame(function update() {
@@ -108,26 +20,11 @@ window.requestAnimFrame = (function () {
         delta = now - before,
         modifier = delta / 1000;
 
-    updateScene(modifier);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (var i = 0; i < projectiles.length; ++i) {
-      projectiles[i].render(ctx);
-    }
-    player.render(ctx);
+    Game.update(modifier);
+    Game.render();
 
     before = now;
     window.requestAnimFrame(update);
   });
-
-  var keysDown = {};
-  document.addEventListener('keydown', function (e) {
-    keysDown = {};
-    keysDown[e.keyCode] = true;
-  }, false);
-
-  document.addEventListener('keyup', function (e) {
-    delete keysDown[e.keyCode];
-  }, false);
 
 }());
