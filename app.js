7c5dbf19cca2b2613e94f60d4647c01b750d5caf
Bruno Sanches <brunopsanches@gmail.com>
2014-03-16 03:25:25 -0700

    Display message when server starts

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-// var sio = require('socket.io');
 var http = require('http');
 var send = require('send');
 
 var server = http.createServer(function (req, res) {
   send(req, req.url).root(__dirname + '/public/').pipe(res);
 }).listen(process.env.PORT || 3000);
+
+
+
+
+console.log("Server listening on port 3000");
